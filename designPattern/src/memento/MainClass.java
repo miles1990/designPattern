7ad08b40047d80e97cd73c1f239b6666d81db03a
miles1990 <gunone@qq.com>
2018-07-27 11:22:39 +0800

    备忘录模式

--- designPattern/src/memento/MainClass.java
+++ designPattern/src/memento/MainClass.java
@@ -5,20 +5,23 @@ public class MainClass {
         Person person=new Person("miles","男",28);
 
 //        Memento memento=person.creatMemento();
+        //备份管理者
         Caretaker caretaker=new Caretaker();
-        caretaker.setMemento(person.creatMemento());
 
+        //进行备份
+        caretaker.setMemento(person.creatMemento());
         person.display();
         System.out.println("-------------");
 
+        //对对象进行修改
         person.setName("xia");
         person.setAge(25);
         person.setSex("女");
         person.display();
         System.out.println("-------------");
 
+        //载入备份
         person.setMemento(caretaker.getMemento());
-        
         person.display();
     }
 }
