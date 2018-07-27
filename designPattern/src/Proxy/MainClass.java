bdc44ec717edbce76d7f7f7622063af0a429d6eb
miles1990 <gunone@qq.com>
2018-07-27 11:29:09 +0800

    代理模式

--- designPattern/src/Proxy/MainClass.java
+++ designPattern/src/Proxy/MainClass.java
@@ -4,9 +4,12 @@ import java.lang.reflect.Proxy;
 
 public class MainClass {
     public static void main(String[] args) {
+        //被代理人
         RealSubject realSubject=new RealSubject();
+        //代理人
         MyHandler myHandler=new MyHandler();
         myHandler.setSubject(realSubject);
+        //使用反射进行方法拦截
         Subject proxySubject=(Subject) Proxy.newProxyInstance(RealSubject.class.getClassLoader(),realSubject.getClass().getInterfaces(),myHandler);
         proxySubject.sailBook();
     }
