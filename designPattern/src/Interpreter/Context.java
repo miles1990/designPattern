07718886fe470c09a2a693ba6b8ec5037cb1dce2
miles1990 <gunone@qq.com>
2018-07-27 16:37:15 +0800

    解释器模式

---
+++ designPattern/src/Interpreter/Context.java
@@ -0,0 +1,22 @@
+package Interpreter;
+
+public class Context {
+    private String input;
+    private int output;
+
+    public String getInput() {
+        return input;
+    }
+
+    public int getOutput() {
+        return output;
+    }
+
+    public void setInput(String input) {
+        this.input = input;
+    }
+
+    public void setOutput(int output) {
+        this.output = output;
+    }
+}
