2225a1b1792e0dedf651c1a3a5b0d19cdef9848b
miles1990 <gunone@qq.com>
2018-07-27 11:12:31 +0800

    观察者模式

--- designPattern/src/Observer/MainClass.java
+++ designPattern/src/Observer/MainClass.java
@@ -3,8 +3,10 @@ package Observer;
 public class MainClass {
     public static void main(String[] args) {
         Person person=new Person();
+        
         //注册观察者
         person.addObserver(new MyObServer());
+
         person.setName("miles");
         person.setSex("男");
         person.setAge(23);
