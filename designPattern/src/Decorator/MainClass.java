bddb066fd325b8290911469e8a02d381ed88f57c
miles1990 <gunone@qq.com>
2018-07-27 11:07:01 +0800

    装饰模式

--- designPattern/src/Decorator/MainClass.java
+++ designPattern/src/Decorator/MainClass.java
@@ -5,14 +5,17 @@ public class MainClass {
         Car runCar=new RunCar();
         runCar.show();
         System.out.println("-------------");
+
         //把飞行功能装饰上去
         Car flyCar=new FlyCarDecorator(runCar);
         flyCar.show();
         System.out.println("-------------");
+
         //把游泳功能装饰上去
         Car swimCar=new SwimCarDecorator(runCar);
         swimCar.show();
         System.out.println("-------------");
+        
         //把上面两个功能都装饰上去
         CarDecorator flyswimCar=new FlyCarDecorator(swimCar);
         flyswimCar.show();
